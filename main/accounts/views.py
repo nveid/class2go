865df379c7a333b3489920d854d20933c1864f45
Jason Bau <jbau@stanford.edu>
2012-09-19 16:22:50 -0700

    lots of shib changes, packaging it up for #310

--- main/accounts/views.py
+++ main/accounts/views.py
@@ -1,5 +1,9 @@
 # Create your views here.
 #from django import form
+
+import urlparse
+import settings
+
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.template import Context, loader, RequestContext
@@ -9,9 +13,15 @@ from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render_to_response
 from django.contrib.auth import logout
-
-from c2g.models import Course
+from django.views.decorators.http import require_POST
+from django.contrib.auth import REDIRECT_FIELD_NAME, login as auth_login, logout as auth_logout
+from django.contrib import messages
+from django.contrib.auth.models import User, Group
+from c2g.models import Course, Institution
 from accounts.forms import *
+import random
+import os
+import base64
 
 def index(request):
     return HttpResponse("Hello, world. You're at the user index.")
@@ -60,10 +70,69 @@ def register(request, template_name='accounts/register.html'):
     });
     return HttpResponse(t.render(c))
 
-    
+@require_POST
+@csrf_protect
 @never_cache
 def shib_login(request):
-    string = ""
-    for k in request.META:
-        string += k + " : " + str(request.META[k]) + "<br />"
-    return HttpResponse(string)
+    
+    #check if there is valid remote user.
+    #if one exists, try to match them
+    #if one does not exist, create it and assign to proper institution
+    #then redirect
+    
+    #setup the redirect first: code borrowed from django contrib library
+    redir_to = request.GET.get('next', '/accounts/profile')
+    netloc = urlparse.urlparse(redir_to)[1]
+       
+    # Heavier security check -- don't allow redirection to a different
+    # host.
+    if netloc and netloc != request.get_host():
+        redir_to = '/accounts/profile'
+                
+    #Use EduPersonPrincipalName http://www.incommonfederation.org/attributesummary.html#eduPersonPrincipal
+    #as username in our system.  We could support other persistent identifiers later, but it will take some
+    #work
+    if ('REMOTE_USER' in request.META) and ('eppn' in request.META) and (request.META['REMOTE_USER']==request.META['eppn']) and request.META['eppn']:
+        
+        #if we get here, the user has authenticated properly
+        
+        shib = {'givenName':'',
+                'sn':'',
+                'mail':'',
+                'Shib-Identity-Provider':'',}
+        
+        shib.update(request.META)
+            
+        if not User.objects.filter(username=shib['REMOTE_USER']).exists():
+            #here, we need to create the new user
+            ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            rg = random.SystemRandom(random.randint(0,100000))
+            password = (''.join(rg.choice(ALPHABET) for i in range(16))) + '1' #create a random password, which they will never use
+            User.objects.create_user(shib['REMOTE_USER'], shib['mail'], shib[''], password)
+            # authenticate() always has to be called before login(), and
+            # will return the user we just created.
+            new_user = authenticate(username=shib['REMOTE_USER'], password=password)
+
+            new_user.first_name, new_user.last_name = shib['givenName'].capitalize(), shib['sn'],capitalize()
+            new_user.save()
+                
+            profile = new_user.get_profile()
+            if shib['Shib-Identity-Provider'] in ('https://idp-dev.stanford.edu/','https://idp.stanford.edu/'):
+                profile.institution.add(Institution.objects.get(title='Stanford'))
+                profile.save()
+        
+            login(request, new_user)
+
+            signals.user_registered.send(sender=self.__class__,
+                             user=new_user,
+                             request=request)
+
+        else:
+            #User already exists, so log him/her in
+            login(request, User.objects.get(username=shib['REMOTE_USER']))
+            messages.add_message(request,message.SUCCESS, 'You have successfully logged in!')
+
+    else:
+        messages.add_message(request,messages.ERROR, 'WebAuth did not return your identity to us!  Please try logging in again.  If the problem continues please contact class2go-support@cs.stanford.edu')
+
+    return HttpResponseRedirect(redir_to)
